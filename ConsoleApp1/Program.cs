e99f627993e66f002909e829ae806ef874ce13f8
Elev <Elev@TEB125105>
2023-03-03 14:34:06 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿
+ class progrom
+{
+  static void Main()
+    {
+     
+        Console.WriteLine("1. Addera tre tal");
+
+
+        {
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+
+            int tal3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" summan av talen är: " + (tal1 + tal2 + tal2 + tal3));
+        }
+
+
+
+    }
+
+    
+
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
